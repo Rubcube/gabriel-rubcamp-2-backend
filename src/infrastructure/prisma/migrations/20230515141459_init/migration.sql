5fac7e738f955f764a3b6b559a8b8bd562775807
gabrielribeirof-rubcube <132402355+gabrielribeirof-rubcube@users.noreply.github.com>
2023-05-15 17:15:43 +0000

    chore(schema.prisma): fix user.account

---
+++ src/infrastructure/prisma/migrations/20230515141459_init/migration.sql
@@ -0,0 +1,91 @@
+-- CreateEnum
+CREATE TYPE "AccountStatus" AS ENUM ('OPEN', 'CLOSED', 'PENDING_APPROVAL', 'BLOCKED');
+
+-- CreateEnum
+CREATE TYPE "InternalTranferStatus" AS ENUM ('COMPLETED', 'CANCELED', 'REJECTED', 'REFUNDED', 'SCHEDULED');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "birthday" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "document" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "addresses" (
+    "user_id" TEXT NOT NULL,
+    "zipcode" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "street" TEXT NOT NULL,
+    "number" TEXT NOT NULL,
+    "complement" TEXT NOT NULL,
+    "neighborhood" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "addresses_pkey" PRIMARY KEY ("user_id")
+);
+
+-- CreateTable
+CREATE TABLE "accounts" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "balance" INTEGER NOT NULL,
+    "account" TEXT NOT NULL,
+    "agency" TEXT NOT NULL,
+    "status" "AccountStatus" NOT NULL,
+    "transaction_password" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "closed_at" TIMESTAMP(3),
+    "blocked_at" TIMESTAMP(3),
+
+    CONSTRAINT "accounts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "internal_transfers" (
+    "id" TEXT NOT NULL,
+    "sender_account_id" TEXT NOT NULL,
+    "recipient_account_id" TEXT NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "status" "InternalTranferStatus" NOT NULL,
+    "scheduled_to" TIMESTAMP(3),
+    "created_at" TIMESTAMP(3) NOT NULL,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "internal_transfers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_phone_key" ON "users"("phone");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_document_key" ON "users"("document");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "accounts_user_id_key" ON "accounts"("user_id");
+
+-- AddForeignKey
+ALTER TABLE "addresses" ADD CONSTRAINT "addresses_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "accounts" ADD CONSTRAINT "accounts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "internal_transfers" ADD CONSTRAINT "internal_transfers_sender_account_id_fkey" FOREIGN KEY ("sender_account_id") REFERENCES "accounts"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "internal_transfers" ADD CONSTRAINT "internal_transfers_recipient_account_id_fkey" FOREIGN KEY ("recipient_account_id") REFERENCES "accounts"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
