35cfcd57e8edbdd814b19b4eae1b4dcba96d1d1a
gabrielribeirof-rubcube <132402355+gabrielribeirof-rubcube@users.noreply.github.com>
2023-05-15 17:15:08 +0000

    tests: fix userMock

--- tests/helpers/mocks/userMock.ts
+++ tests/helpers/mocks/userMock.ts
@@ -4,13 +4,13 @@ export function userMock() {
 	return {
 		name: faker.person.fullName(),
 		email: faker.internet.email(),
-		birthday: '10-03-2001',
+		birthday: '1998-10-04',
 		phone: {
 			country_code: '55',
 			area_code: '11',
 			number: '997659306'
 		},
-		document: '12345678901',
+		document: '15762348008',
 		password: '12345678aA!',
 		transactional_password: '1234'
 	}
