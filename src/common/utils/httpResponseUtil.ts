a9fec88ec4e665e837d7769689f6f449aba9b343
gabrielribeirof-rubcube <132402355+gabrielribeirof-rubcube@users.noreply.github.com>
2023-05-16 13:43:32 +0000

    feat(httpResponseUtil.ts): add created util

--- src/common/utils/httpResponseUtil.ts
+++ src/common/utils/httpResponseUtil.ts
@@ -1,6 +1,6 @@
 import { Response } from 'express'
-import { AppError } from 'src/common/seedword/errors/AppError'
-import { AppErrorCodes } from 'src/common/seedword/errors/AppErrorCodes'
+import { AppError } from 'common/seedword/errors/AppError'
+import { AppErrorCodes } from 'common/seedword/errors/AppErrorCodes'
 
 import { parseAppErrorToHTTPError } from './parseAppErrorToHttpError'
 
@@ -8,10 +8,16 @@ export function ok(expressResponse: Response, payload?: Record<string, any>): Re
 	return expressResponse.status(200).json(payload)
 }
 
+export function created(expressResponse: Response, payload?: Record<string, any>): Response {
+	return payload ? expressResponse.status(201).json(payload) : expressResponse.status(201).send()
+}
+
 export function fail(expressResponse: Response, payload: AppError): Response {
 	switch (payload.code) {
+		case AppErrorCodes.RESOURCE_NOT_FOUND:
 		case AppErrorCodes.NOT_FOUND:
 			return expressResponse.status(404).send(parseAppErrorToHTTPError(payload))
+		case AppErrorCodes.INVALID_OPERATION:
 		case AppErrorCodes.INVALID_PARAMETER:
 			return expressResponse.status(400).send(parseAppErrorToHTTPError(payload))
 		case AppErrorCodes.INTERNAL_ERROR:
