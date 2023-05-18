0176588f92e928dfcb68455fa6a09cf808da9e00
gabrielribeirof-rubcube <132402355+gabrielribeirof-rubcube@users.noreply.github.com>
2023-05-18 17:29:42 +0000

    feat: register account module repositories in container

--- src/common/container/index.ts
+++ src/common/container/index.ts
@@ -2,12 +2,21 @@ import { container } from 'tsyringe'
 
 import '../providers'
 
-import { IUserRepository } from 'modules/users/domain/user/IUserRepository'
-import { PrismaUserRepository } from 'modules/users/repositories/PrismaUserRepository'
+import { IUserRepository } from 'modules/identity/domain/user/IUserRepository'
+import { PrismaUserRepository } from 'modules/identity/repositories/prisma/PrismaUserRepository'
 
-import { IAccountRepository } from 'modules/users/domain/account/IAccountRepository'
-import { PrismaAccountRepository } from 'modules/users/repositories/PrismaAccountRepository'
+import { IAccountRepository } from 'modules/identity/domain/account/IAccountRepository'
+import { PrismaAccountRepository } from 'modules/identity/repositories/prisma/PrismaAccountRepository'
+
+import { ITransactionRepository } from 'modules/transactions/domain/transaction/ITransactionRepository'
+import { PrismaTransactionRepository } from 'modules/transactions/repositories/PrismaTransactionRepository'
+
+import { IInternalTransferRepository } from 'modules/transactions/domain/internal_transfer/IInternalTransferRepository'
+import { PrismaInternalTransferRepository } from 'modules/transactions/repositories/PrismaInternalTransferRepository'
 
 container.registerSingleton<IUserRepository>('UserRepository', PrismaUserRepository)
 
 container.registerSingleton<IAccountRepository>('AccountRepository', PrismaAccountRepository)
+
+container.registerSingleton<ITransactionRepository>('TransactionRepository', PrismaTransactionRepository)
+container.registerSingleton<IInternalTransferRepository>('InternalTransferRepository', PrismaInternalTransferRepository)
