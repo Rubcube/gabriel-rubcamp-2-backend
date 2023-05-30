8792250434ccab95cc0d38ed505ef5cc8a8f065c
gabrielribeirof-rubcube <132402355+gabrielribeirof-rubcube@users.noreply.github.com>
2023-05-30 15:41:26 +0000

    chore: remove tests

--- src/common/container/index.ts
+++ src/common/container/index.ts
@@ -3,10 +3,10 @@ import { container } from 'tsyringe'
 import '../providers'
 
 import { IUserRepository } from 'modules/identity/domain/user/IUserRepository'
-import { PrismaUserRepository } from 'modules/identity/repositories/prisma/PrismaUserRepository'
+import { PrismaUserRepository } from 'modules/identity/repositories/PrismaUserRepository'
 
 import { IAccountRepository } from 'modules/identity/domain/account/IAccountRepository'
-import { PrismaAccountRepository } from 'modules/identity/repositories/prisma/PrismaAccountRepository'
+import { PrismaAccountRepository } from 'modules/identity/repositories/PrismaAccountRepository'
 
 import { ITransactionRepository } from 'modules/transactions/domain/transaction/ITransactionRepository'
 import { PrismaTransactionRepository } from 'modules/transactions/repositories/PrismaTransactionRepository'
--- tests/helpers/config.ts
+++
@@ -1,3 +0,0 @@
-export const config = {
-	url: 'http://web:3344'
-}
